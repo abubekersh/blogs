665b6e77668894591abb76367c2505b928a09ee9
abubeker <abakabrak@gmail.com>
2024-09-19 01:05:35 +0300

    added  four more routs  for about page and blogs

--- routes.php
+++ routes.php
@@ -1,3 +1,7 @@
 <?php
 
 $router->get("/",'index.php');
+$router->get("/about",'about.php');
+$router->get("/blogs",'blogs/index.php');
+$router->get("/blogs/create",'blogs/create.php');
+$router->post("blogs/",'blogs/store.php');
